9a0c0d8998022bc1ea2d822e95a80682fe6e3155
Ram Chaudhary <121219928+HackerJaatWorld@users.noreply.github.com>
2023-01-18 20:43:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+
+hamMenuIcon.addEventListener("click", () => {
+    navBar.classList.toggle("active");
+    hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+    navLinks.addEventListener("click", () => {
+        navBar.classList.remove("active");
+        hamMenuIcon.classList.toggle("fa-times");
+    });
+});
+
+
+
+// ==============text=================
+
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'Tutorials With Ram Chaudhary'
+let idx = 1
+let speed = 300 / speedEl.value
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if (idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+}
+
+
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
