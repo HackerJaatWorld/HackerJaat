9774f387d1a0f72ffa879c8c36c238a435b6552b
Ram Chaudhary <121219928+HackerJaatWorld@users.noreply.github.com>
2023-01-25 13:07:58 +0530

    Add files via upload

---
+++ big-projects/songi/js/music-list.js
@@ -0,0 +1,79 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "Agar Tum Mil Jao",
+        artist: "Unknown1",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Kuchh To Hai Tujhse Rabta",
+        artist: "Unknown2",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Hmen Tumse Hua Hai Pyar",
+        artist: "Unknown3",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Tumko Paya Hai To Jese",
+        artist: "Unknown4",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Vo Ladki Nhi Jindgi Hai Meri",
+        artist: "Unknown5",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Rafta Rafta Sanam",
+        artist: "Unknown6",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Ey Jate Huye Lamhe",
+        artist: "Unknown7",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Gulab Kaka New Song ",
+        artist: "Unknown8",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Ni Me Daku ",
+        artist: "Unknown9",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Top Hindi Songs 2023",
+        artist: "Unknown10",
+        img: "music-10",
+        src: "music-10"
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+];
